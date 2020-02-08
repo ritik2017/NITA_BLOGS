64c6a7d01dc8f4da2d4aade00ade0d096223c3f9
ritik2017 <ritikkumarrrp@gmail.com>
2020-02-08 23:17:37 +0530

    Added sessions for users

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = require('./router')
+const session = require('express-session')
 
 const app = express()
+
+let sessionOptions = session({
+    secret: "This is NITA BLOGS site",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
